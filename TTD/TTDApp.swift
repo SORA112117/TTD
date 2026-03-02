62ab53827e1d3cf1570696429384cfb4f0a58be6
sora <so.ra13011217@icloud.com>
2026-03-03 01:31:56 +0900

    Initial Commit

---
+++ TTD/TTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTDApp.swift
+//  TTD
+//
+//  Created by 山内壮良 on 2026/03/03.
+//
+
+import SwiftUI
+
+@main
+struct TTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TTDTests/TTDTests.swift
@@ -0,0 +1,17 @@
+//
+//  TTDTests.swift
+//  TTDTests
+//
+//  Created by 山内壮良 on 2026/03/03.
+//
+
+import Testing
+@testable import TTD
+
+struct TTDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
